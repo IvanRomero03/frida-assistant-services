1e8f990b6b54cc29e0d174b217b3b28688082d32
Chapa-1810 <alexischaparomero@gmail.com>
2023-09-23 19:22:34 -0600

    web_scrapper && pdf_scrapper

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
-
 from flask import Flask, request, jsonify
+from PyPDF2 import PdfReader
+import gcsfs
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
@@ -15,3 +18,36 @@ def not_found(error):
 @app.errorhandler(500)
 def server_error(error):
     return jsonify({"error": "Internal server error"}), 500
+
+@app.route("/api/pdf_scrapper")
+def parse_pdf(gcs_pdf_path):
+    #works url gsutil url
+    gcs_file_system = gcsfs.GCSFileSystem(project="frida-file-bucket", token="credentials.json")
+
+    f_object = gcs_file_system.open(gcs_pdf_path, "rb")
+    pdf = PdfReader(f_object)
+    number_of_pages = len(pdf.pages)
+    parsed_text = ""
+
+    for i in range (number_of_pages):
+        page = pdf.pages[i]
+        parsed_text = parsed_text + page.extract_text() + "\n"
+    
+    return jsonify({"text": parsed_text})
+
+@app.route("/api/web_scrapper")
+# URL of the page to be scraped
+def scrap_text(url):
+    # Retrieve page with the requests module
+    response = requests.get(url)
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    parsed_text = ""
+    # Print all paragraph texts
+    for par in soup.find_all(['h1','h2','p','table']):
+        parsed_text = parsed_text + par.name + " : " + par.text + "\n" 
+
+    return jsonify({"text": parsed_text})    
+
